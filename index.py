bca2c5c0d024a529426cecfb35cd3e374e4d1219
Trevor Wolfe <trevo1992@gmail.com>
2016-11-28 15:10:23 -0800

    ...

--- index.py
+++ index.py
@@ -38,13 +38,13 @@ async def on_message(message):
             pass
     if message.content.startswith('!v '):
         if message.content.startswith('!v joinchannel'):
-            voice_channel_service = await voice.voice()
-            await voice_channel_service.joinchannel(client,message)
+            voice_channel_service = voice.voice()
+            voice_channel_service.joinchannel(client,message)
         if message.content.startswith('!v leave'):
-            await voice_channel_service.leave()
+            voice_channel_service.leave()
         if message.content.startswith('!v playsong'):
             song_to_play = message.content[13:]
-            await voice_channel_service.playsong(song_to_play)
+            voice_channel_service.playsong(song_to_play)
     for role_search in message.role_mentions:
         if role_search.name == 'Nickelback':
             await commands.nickelback(client, message)
