8469d74e4ea31c20487d7ca5e7b6f05bfddc932a
Trevor Wolfe <trevo1992@gmail.com>
2016-11-29 17:18:41 -0800

    wtf

--- voice.py
+++ voice.py
@@ -8,12 +8,12 @@ class voice:
     def init(self, client, message):
         self.server = ""
 
-    def joinchannel(client,message):
+    def joinchannel(self,client,message):
         self.voice = client.join_voice_channel(message.author.voice_channel)
 
-    def leave():
+    def leave( self):
         self.voice.disconnect()
 
-    def playsong(url):
+    def playsong(self,url):
         self.player = voice.create_ytdl_player(url)
         self.player.start()
