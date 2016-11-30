115b1d705731f3734345ea2b8d378860e017c0da
Trevor Wolfe <trevo1992@gmail.com>
2016-11-29 17:22:59 -0800

    does this even work

--- voice.py
+++ voice.py
@@ -9,17 +9,6 @@ class voice:
         self.client = client
         self.message = message
 
-<<<<<<< HEAD
-    def joinchannel(self,client,message):
-        self.voice = client.join_voice_channel(message.author.voice_channel)
-
-    def leave( self):
-        self.voice.disconnect()
-
-    def playsong(self,url):
-        self.player = voice.create_ytdl_player(url)
-        self.player.start()
-=======
     async def joinchannel(self):
         try:
             self.voice = await self.client.join_voice_channel(message.author.voice_channel)
@@ -35,4 +24,3 @@ class voice:
     async def playsong(url):
         await self.player = self.voice.create_ytdl_player(url)
         await self.player.start()
->>>>>>> acfd77d169b122452346958ef05fad10890842e3
