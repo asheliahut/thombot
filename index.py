888b95d71b211d8e8d635c7da3d7c988567cb616
Trevor Wolfe <trevo1992@gmail.com>
2016-11-29 17:48:45 -0800

    does this even work

--- index.py
+++ index.py
@@ -40,13 +40,13 @@ async def on_message(message):
             pass
     if message.content.startswith('!v '):
         if message.content.startswith('!v joinchannel'):
-            voice_channel_service = voice.voice(client, message)
-            voice_channel_service.joinchannel()
+            voice_channel = voice.voice(client, message)
+            await voice_channel.joinchannel()
         if message.content.startswith('!v leave'):
-            voice_channel_service.leave()
+            voice_channel.leave()
         if message.content.startswith('!v playsong'):
             song_to_play = message.content[13:]
-            voice_channel_service.playsong(song_to_play)
+            voice_channel.playsong(song_to_play)
     for role_search in message.role_mentions:
         if role_search.name == 'Nickelback':
             await commands.nickelback(client, message)
