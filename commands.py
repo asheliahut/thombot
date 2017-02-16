9f30f8dab237cc5e9b333a8d42c57cf7902faff6
Trevor Wolfe <trevo1992@gmail.com>
2017-02-16 15:40:57 -0800

    added doomfist and ban command

--- commands.py
+++ commands.py
@@ -93,10 +93,10 @@ async def stfu(client, message):
     await client.send_message(message.channel, 'https://youtu.be/OLpeX4RRo28')
 
 async def doomfist(client, message):
-    await client.send_file(message.channel, '/home/pi/thombot/picutres/doomfist.jpg')
+    await client.send_file(message.channel, '/home/pi/thombot/pictures/doomfist.jpg')
 
 async def ban(client, message):
-    await client.send_message(message.channel, 'you\'ve been banned, ' + message.mentions[1].mention)
+    await client.send_message(message.channel, 'you\'ve been banned, {}!'.format(message.mentions[1].mention))
 
 #async def test(client, message):
     #for channel in message.server.channels:
